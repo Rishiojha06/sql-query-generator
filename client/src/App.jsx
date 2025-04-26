da9a9db6995a6bf74d873d9297a844d53a54c185
Rishi Ojha <rishiojha0606@gmail.com>
2025-04-26 17:59:49 +0530

    feature(client): create react component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-
+import { use, useState } from 'react'
+import styles from './index.module.css'
+import sqlLogo from './assets/sql-logo.png'
 function App() {
+  const [queryDescription, setQueryDescription] = useState("")
+  console.log(queryDescription)
+
   return (
-    <>
-    <h1>This is H1</h1>
-    </>
+    <main className={styles.main}>
+      <img src={sqlLogo} alt='' className={styles.icon} />
+      <h3>Generate SQL with AI</h3>
+      <form action="" onSubmit={(event) => event.preventDefault()}>
+        <input type="text" name='query-description' placeholder='Describe your query' onChange={(event) => setQueryDescription(event.target.value)} />
+        <input type="submit" value="Submit query" />
+      </form>
+    </main>
   )
 }
 
